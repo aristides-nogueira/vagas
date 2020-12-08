115eb6fd397ddb61daed8851cdc24475581ca2d7
Rod <rodfeal@gmail.com>
2020-12-07 23:57:04 -0300

    Updates

---
+++ precache-manifest.0d6407300764781b11947ae0797ce442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df736fbc1ed2fd8517903445b35f263",
+    "url": "/vagas/index.html"
+  },
+  {
+    "revision": "6fbdd5f7b6768bb3dc13",
+    "url": "/vagas/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30f0573aa2a8c46be276",
+    "url": "/vagas/static/js/2.61980695.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/vagas/static/js/2.61980695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fbdd5f7b6768bb3dc13",
+    "url": "/vagas/static/js/main.c7b445f2.chunk.js"
+  },
+  {
+    "revision": "7804c36289efe99ddcf8",
+    "url": "/vagas/static/js/runtime-main.a5da444d.js"
+  }
+]);
